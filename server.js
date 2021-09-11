c6ed87383d088725d08bf9717681619aa83c4bf9
shree675 <shreeteshm37@gmail.com>
2021-09-11 23:55:00 +0530

    added primary route

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+//@ts-check
+
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+
+app.get("/", (req, res) => {
+    app.use(express.static("./client/src/App.js"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.get("/api/customers", cors(), (req, res) => {
     const customers = [
         { id: 1, firstName: "John", lastName: "Doe" },
-        { id: 2, firstName: "Brad", lastName: "Traversy" },
+        { id: 2, firstName: "Brad", lastName: "Traversy Abc" },
         { id: 3, firstName: "Mary", lastName: "Swanson" },
     ];
 
