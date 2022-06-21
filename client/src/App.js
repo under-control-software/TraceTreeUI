44cdc0ef0a2ee656ddbe5e64eec9ae416279d4fd
Divyansh <divyansh.24888@gmail.com>
2022-06-21 13:44:22 +0530

    More UI changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,12 @@ import React, { Component, useState } from "react";
 import "./App.css";
 import { Radio } from "antd";
 import Graph from "react-graph-network";
-import { MagnifyingGlass } from "phosphor-react";
+import {
+  MagnifyingGlass,
+  TreeStructure,
+  File,
+  ArrowRight,
+} from "phosphor-react";
 
 const Line = ({ link, ...restProps }) => {
   return <line {...restProps} stroke="grey" />;
@@ -249,7 +254,7 @@ class App extends Component {
               placeholder="Number of Arguments"
             />
             <button className="run-button" onClick={this.run}>
-              Run
+              <TreeStructure size={25} weight="bold" />
             </button>
           </div>
         </div>
@@ -274,7 +279,7 @@ class App extends Component {
               />
             </div>
             <div className="right-box">
-              <div style={{ fontSize: "1.2em" }}>
+              <div style={{ fontSize: "1.2em", color: "white" }}>
                 <b>Please select the correct reference</b>
               </div>
               <div className="radio-buttons">
@@ -288,14 +293,17 @@ class App extends Component {
                         return (
                           <div
                             style={{
-                              backgroundColor: ind % 2 ? "#408ffd3a" : "white",
-                              padding: "0.2rem",
                               border:
                                 this.state.radioValue === e
                                   ? "2px solid #408efd"
                                   : "",
-                              borderRadius: "5px",
+                              // add a glowing shadow
+                              boxShadow:
+                                this.state.radioValue === e
+                                  ? "0px 0px 10px #408efd"
+                                  : "",
                             }}
+                            className={`card card-${Math.floor(ind % 5) + 1}`}
                           >
                             <Radio
                               value={e}
@@ -307,23 +315,23 @@ class App extends Component {
                               }}
                             >
                               {" "}
-                              File: {e.file}
-                              <div
-                                style={{
-                                  fontSize: "0.88em",
-                                  marginTop: "3px",
-                                  marginLeft: "28px",
-                                  paddingBottom: "8px",
-                                }}
-                              >
+                              <div className="file-name-cont">
+                                <File
+                                  size={18}
+                                  className="card__icon"
+                                  weight="bold"
+                                />{" "}
+                                {e.file}
+                              </div>
+                              <div className="file-link-cont">
+                                <span className="preview">{e.preview}...</span>
                                 <a
                                   className="code-prev"
                                   href={e.url}
                                   target="_blank"
                                 >
-                                  Line
+                                  <ArrowRight size={20} weight="bold" />
                                 </a>
-                                : {e.preview}...
                               </div>
                             </Radio>
                           </div>
